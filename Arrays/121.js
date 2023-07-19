089c715fac9388143ce6565a11362b6acf94cba0
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-07-20 00:47:49 +0530

    add: Best Time to Buy and Sell Stock

---
+++ Arrays/121.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let cur_min = prices[0];
+  let cur_profit = 0;
+  for (let i = 1; i < prices.length - 1; i++) {
+    // Can I buy?
+    if (prices[i] < cur_min) cur_min = prices[i];
+    // Can I sell?
+    if (prices[i] > cur_min)
+      cur_profit = Math.max(cur_profit, prices[i] - cur_min);
+  }
+  return cur_profit;
+};
+
+prices = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices));
