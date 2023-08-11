1ef18b640f6bec54c0e6c2eb8ab300302920fc7e
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-08-11 21:00:43 +0530

    fix: allow users to specify the return types

--- Core/Practice Questions/debounce.ts
+++ Core/Practice Questions/debounce.ts
@@ -1,11 +1,14 @@
-type DebounceFunction = (func: (...args: any[]) => void, delay: number) => (...args: any[]) => void;
+type DebounceFunction = <T extends (...args: any[]) => any>(func: T, delay: number) => (...args: Parameters<T>) => Promise<ReturnType<T>>;
 
 const DebounceIt: DebounceFunction = (func, delay) => {
     let timeoutId: ReturnType<typeof setTimeout>;
-    return (...args: any[]) => {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => func(...args), delay);
-    }
+    return (...args: any[]) =>
+        new Promise<ReturnType<typeof func>>((resolve) => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                resolve(func(...args));
+            }, delay);
+        });
 }
 
 const printDebouncedName = DebounceIt((input: string) => { console.log(`Debounce: ${input}`) }, 300);
