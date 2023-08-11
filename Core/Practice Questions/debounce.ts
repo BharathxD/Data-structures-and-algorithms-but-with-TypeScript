7e1dbea280910ea5124875b576b23352ab99680a
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-08-11 21:09:01 +0530

    chore: add more descriptive names

--- Core/Practice Questions/debounce.ts
+++ Core/Practice Questions/debounce.ts
@@ -1,18 +1,15 @@
 type DebounceFunction = <T extends (...args: any[]) => any>(func: T, delay: number) => (...args: Parameters<T>) => Promise<ReturnType<T>>;
 
-const DebounceIt: DebounceFunction = (func, delay) => {
+const debounce: DebounceFunction = (func, delay) => {
     let timeoutId: ReturnType<typeof setTimeout>;
-    return (...args: any[]) =>
-        new Promise<ReturnType<typeof func>>((resolve) => {
-            clearTimeout(timeoutId);
-            timeoutId = setTimeout(() => {
-                resolve(func(...args));
-            }, delay);
-        });
+    return (...args: any[]) => new Promise<ReturnType<typeof func>>((resolve) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => resolve(func(...args)), delay)
+    })
 }
 
-const printDebouncedName = DebounceIt((input: string) => { console.log(`Debounce: ${input}`) }, 300);
+const debouncedFunction = debounce((input: string) => console.log(`debounce: ${input}`), 300);
 
-printDebouncedName('A');
-printDebouncedName('B');
-printDebouncedName('C');
+debouncedFunction('A');
+debouncedFunction('B');
+debouncedFunction('C');
