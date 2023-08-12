798ea77ea1a0ff34053ae3b394b0a496267305d9
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-08-12 22:53:00 +0530

    chore: improve documentation

--- Core/Practice Questions/selection-sort.ts
+++ Core/Practice Questions/selection-sort.ts
@@ -5,8 +5,11 @@ export function swap(i: number, j: number, arr: number[]) {
 }
 
 export default function insertionSort(arr: Array<number>): Array<number> {
+    // Iterate over each elements in the given arr
     for (let i = 0; i < arr.length; i++) {
+        // Now start from the index that we're on, and start iterating from that index to the end of the array
         for (let j = i; j < arr.length; j++) {
+            // Check if the each element in the given array is equal to element that we're iterating
             if (arr[j] < arr[i]) swap(i, j, arr);
         }
     }
