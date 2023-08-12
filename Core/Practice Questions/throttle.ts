3d0e263f0d918b39bfbae6a378c97ad081270db3
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-08-12 21:18:52 +0530

    add: throttle

---
+++ Core/Practice Questions/throttle.ts
@@ -0,0 +1,18 @@
+type ThrottleFunction = <T extends (...args: any[]) => any>(func: T, delay: number) => (...args: Parameters<T>) => Promise<ReturnType<T>>;
+
+const throttle: ThrottleFunction = (func, delay) => {
+    let flag: boolean = true;
+    return (...args: any[]) => new Promise<ReturnType<typeof func>>((resolve) => {
+        if (flag) {
+            flag = false;
+            resolve(func(...args));
+            setTimeout(() => flag = true, delay);
+        }
+    })
+}
+
+const expensiveFunction = throttle((input: string) => console.log(input), 300);
+
+expensiveFunction('A');
+expensiveFunction('B');
+expensiveFunction('C');
