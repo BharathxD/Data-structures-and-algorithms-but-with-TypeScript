0231ac135512d903d0c029e5d10c9d7253a0d7f4
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-08-14 00:02:30 +0530

    chore: improve documentation

--- Core/Multi-Threading/src/run.ts
+++ Core/Multi-Threading/src/run.ts
@@ -1,13 +1,24 @@
 import { Worker } from "worker_threads";
 import chunkify from "./utils/chunkify";
 
-const runConcurrentJobs = (jobs: number[], concurrentWorkers: number) => {
+/**
+ * Run multiple jobs concurrently using worker threads.
+ * @param jobs - An array of job numbers to process concurrently.
+ * @param concurrentWorkers - The number of concurrent worker threads to use.
+ */
+const runConcurrentJobs = (jobs: number[], concurrentWorkers: number): void => {
     const start = performance.now();
+
+    // Divide the jobs into chunks for concurrent processing.
     const chunks = chunkify(jobs, concurrentWorkers);
+
+    // Create and launch worker threads for each chunk of jobs.
     chunks.forEach((chunk, i) => {
         const worker = new Worker('./dist/workers/worker.js');
+        // Send the chunk of jobs to the worker thread.
         worker.postMessage(chunk);
     });
+
     const end = performance.now();
     console.log(`${concurrentWorkers} Workers took: ${end - start}ms`);
     process.exit(0);
