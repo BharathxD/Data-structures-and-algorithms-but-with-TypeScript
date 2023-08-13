6989ae663b7c20a885fdbb4581f0bf94e6276bc6
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-08-13 21:06:13 +0530

    chore: add documentation

--- Core/Multi-Threading/src/index.ts
+++ Core/Multi-Threading/src/index.ts
@@ -1,6 +1,15 @@
 import runConcurrentJobs from "./run";
 
-const billion = 1e9;
-const jobs: number[] = Array.from({ length: 100 }, () => billion);
+const oneBillion = 1e9;
+/**
+ * The following variable `jobs` contain, one hundered 1e9's
+ * [
+ *   1_000_000_000,
+ *   1_000_000_000,
+ *   1_000_000_000,
+ *   ...
+ * ]
+ */
+const jobs: number[] = Array.from({ length: 100 }, () => oneBillion);
 
 runConcurrentJobs(jobs, 8);
