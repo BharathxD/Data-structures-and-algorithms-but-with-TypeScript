d4c431729c143c3b45ce9d7024af1c2576a8c4f4
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-08-13 21:04:13 +0530

    add: multi-threaded program

--- Core/Multi-Threading/src/run.ts
+++ Core/Multi-Threading/src/run.ts
@@ -3,22 +3,14 @@ import chunkify from "./utils/chunkify";
 
 const runConcurrentJobs = (jobs: number[], concurrentWorkers: number) => {
     const start = performance.now();
-    let completedWorkers = 0;
     const chunks = chunkify(jobs, concurrentWorkers);
     chunks.forEach((chunk, i) => {
         const worker = new Worker('./dist/workers/worker.js');
         worker.postMessage(chunk);
-        worker.on('message', () => {
-            console.log(`Worker ${i} completed`);
-            completedWorkers++;
-            if (completedWorkers === concurrentWorkers) {
-                console.log(`${concurrentWorkers} workers took ${performance.now()} ms`)
-                process.exit();
-            }
-        })
-    })
+    });
     const end = performance.now();
-    console.log(`${concurrentWorkers} Workers took: ${end - start}ms`)
+    console.log(`${concurrentWorkers} Workers took: ${end - start}ms`);
+    process.exit(0);
 }
 
 export default runConcurrentJobs;
--- Core/Multi-Threading/src/workers/worker.ts
+++ Core/Multi-Threading/src/workers/worker.ts
@@ -1,12 +1,11 @@
 import { parentPort } from "worker_threads";
 
-parentPort?.on('message', (jobs) => {
+parentPort.on('message', (jobs) => {
     for (let job of jobs) {
         let count = 0;
         for (let i = 0; i < job; i++) {
             count++;
         }
     }
-    // Send data back to the main thread
-    parentPort?.postMessage('done');
+    process.exit(0);
 })
