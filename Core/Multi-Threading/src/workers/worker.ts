72acb202a4b4f02fd6070c2952cf257af42d8a9d
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-08-14 18:16:36 +0530

    chore: improve documentation

--- Core/Multi-Threading/src/workers/worker.ts
+++ Core/Multi-Threading/src/workers/worker.ts
@@ -1,11 +1,17 @@
 import { parentPort } from "worker_threads";
 
-parentPort.on('message', (jobs) => {
+/**
+ * Listens for messages from the parent thread and processes each job.
+ * 
+ * @param jobs - An array of jobs to be processed.
+ */
+parentPort.on('message', (jobs: number[]) => {
     for (let job of jobs) {
         let count = 0;
         for (let i = 0; i < job; i++) {
             count++;
         }
     }
+    // Signal that the processing is complete and exit the worker thread.
     process.exit(0);
-})
+});
