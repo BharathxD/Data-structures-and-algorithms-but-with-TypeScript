a7692acf56893c17b802b37a7342bc8a1ec8f67a
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-08-14 18:15:27 +0530

    chore: improve documentation

--- Core/Multi-Threading/src/utils/chunkify.ts
+++ Core/Multi-Threading/src/utils/chunkify.ts
@@ -1,7 +1,15 @@
-const chunkify = (array: number[], n: number) => {
-    let chunks = [];
+/**
+ * Divides an array into multiple chunks, attempting to evenly distribute the elements.
+ *
+ * @param array - The array of numbers to be divided into chunks.
+ * @param n - The number of chunks to create.
+ * @returns An array containing the divided chunks.
+ */
+const chunkify = (array: number[], n: number): number[][] => {
+    const chunkSize = Math.ceil(array.length / n);
+    let chunks: number[][] = [];
     for (let i = n; i > 0; i--) {
-        chunks.push(array.splice(0, Math.ceil(array.length / i)))
+        chunks.push(array.splice(0, chunkSize));
     }
     return chunks;
 }
