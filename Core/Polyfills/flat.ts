9acdc7934a61cc34a46f5e5f0aeef2949f8eef38
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-08-15 22:39:20 +0530

    chore: improve documentation

--- Core/Polyfills/flat.ts
+++ Core/Polyfills/flat.ts
@@ -1,5 +1,14 @@
 const nestedArray = [1, 2, [3, 4, [5, 6, 7]], 5, [6]];
 
+/**
+ * Flattens a nested array to a one-dimensional array.
+ *
+ * This function recursively flattens a nested array structure, returning
+ * a one-dimensional array containing all the elements from the input array.
+ *
+ * @param arr - The array to be flattened.
+ * @returns A one-dimensional array containing all the elements from the input array.
+ */
 const flatten = (arr: any[]): number[] => arr.reduce((acc, curr) => Array.isArray(curr) ? acc.concat(flatten(curr)) : acc.concat(curr), []);
 
 console.log(`Flat polyfill: ${flatten(nestedArray)}`);
