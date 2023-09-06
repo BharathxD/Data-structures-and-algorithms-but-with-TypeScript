bf531539eb5f72e0d3cbe81309b54a54b218fcd5
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-09-06 21:30:51 +0530

    chore: improve documentation for maxProfit function

--- Core/Leetcode/array/121.js
+++ Core/Leetcode/array/121.js
@@ -1,6 +1,11 @@
 /**
- * @param {number[]} prices
- * @return {number}
+ * Calculates the maximum profit that can be obtained by buying and selling stocks
+ * given an array of stock prices.
+ *
+ * @param {number[]} prices - An array of stock prices where prices[i] represents
+ * the price of the stock on the i-th day.
+ *
+ * @return {number} - The maximum profit that can be obtained.
  */
 var maxProfit = function (prices) {
   let cur_min = prices[0];
