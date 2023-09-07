54d497412664d23737670c712bed2eec3f8e08b4
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-09-07 19:59:02 +0530

    chore: improve documentation for the singleNumber problem

--- Arrays/[136] Single Number/index.ts
+++ Arrays/[136] Single Number/index.ts
@@ -1,15 +1,20 @@
 /**
- * @url https://leetcode.com/problems/single-number/submissions/
- * @param {number[]} nums
- * @return {number}
+ * LeetCode URL: https://leetcode.com/problems/single-number/submissions/
+ *
+ * Finds the single number in an array where all other elements appear twice.
+ *
+ * @param {number[]} nums - An array of integers.
+ * @param {number} result - The initial value for the result (default is 0).
+ * @param {number} i - The current index while recursively processing the array (default is 0).
+ * @return {number} - The single number in the array.
  */
-function singleNumber(nums: number[], result: number = 0, i = 0): number {
+function singleNumber(nums, result = 0, i = 0) {
     if (i === nums.length) return result;
     return singleNumber(nums, result ^= nums[i], ++i);
-};
+}
 
 /**
  * Test Cases
  */
-const nums = [2, 2, 1]
+const nums = [2, 2, 1];
 singleNumber(nums);
