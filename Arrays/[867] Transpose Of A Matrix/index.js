2fb3a47b88ccc54f0e55cd1f8138d899f5ac34e2
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-09-07 19:57:02 +0530

    chore: improve documentation for the reshape matrix problem

--- Arrays/[867] Transpose Of A Matrix/index.js
+++ Arrays/[867] Transpose Of A Matrix/index.js
@@ -1,19 +1,25 @@
 /**
- * @param {number[][]} matrix
- * @return {number[][]}
+ * Transposes a matrix (2D array).
+ *
+ * @param {number[][]} matrix - The input matrix to be transposed.
+ * @return {number[][]} - The transposed matrix.
  */
-
 var transpose = (matrix) => {
+  // Create a new matrix with swapped dimensions
   let result = Array(matrix[0].length);
+
   for (let i = 0; i < matrix[0].length; i++) {
     let arr = Array(matrix.length);
     result[i] = arr;
   }
+
+  // Copy elements from the original matrix to the transposed matrix
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       result[j][i] = matrix[i][j];
     }
   }
+
   return result;
 };
 
