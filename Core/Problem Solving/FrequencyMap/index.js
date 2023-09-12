3abfd68ff1c6fc9f98eb4503bf19e21d06e80350
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-09-12 21:37:59 +0530

    add: documentation to the frequency map problem

--- Core/Problem Solving/FrequencyMap/index.js
+++ Core/Problem Solving/FrequencyMap/index.js
@@ -1,3 +1,8 @@
+/**
+ * Counts the frequency of each unique character in a string.
+ * @param {string} value - The input string.
+ * @returns {Object} An object where keys are unique characters, and values are their frequencies.
+ */
 const frequency = (value) => {
   let obj = {};
   for (let val of value) {
@@ -10,6 +15,11 @@ const frequency = (value) => {
   return obj;
 };
 
+/**
+ * Returns an array of unique values from an input array.
+ * @param {Array} arrayOfDuplicateValues - The input array with possible duplicate values.
+ * @returns {Array} An array containing unique values from the input array.
+ */
 const uniqueKeys = (arrayOfDuplicateValues) => {
   const nonDuplicateValues = {};
   for (const property of arrayOfDuplicateValues) {
@@ -21,5 +31,4 @@ const uniqueKeys = (arrayOfDuplicateValues) => {
 };
 
 console.log(uniqueKeys(["a", "b", "c", "d", "b", "c", "g", "e"]));
-
 console.log(frequency("javascript"));
