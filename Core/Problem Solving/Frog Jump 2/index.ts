0b692913b6efd8edcd6e33fb43160398809d6574
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-09-12 21:39:36 +0530

    add: documentation to the frog jump 2 problem

--- Core/Problem Solving/Frog Jump 2/index.ts
+++ Core/Problem Solving/Frog Jump 2/index.ts
@@ -1,3 +1,15 @@
+/**
+ * Calculates the minimum cost to reach the end from the start index
+ * by making at most 'k' jumps while considering the cost of jumping
+ * between stones in an array.
+ *
+ * @param {number} start - The starting index.
+ * @param {number} end - The ending index.
+ * @param {number} k - The maximum number of jumps allowed.
+ * @param {number[]} stones - An array of stone positions.
+ * @param {number[][]} memo - A memoization table to store computed results.
+ * @returns {number} The minimum cost to reach the end.
+ */
 const calculateMinimumCost = (
     start: number,
     end: number,
@@ -5,13 +17,18 @@ const calculateMinimumCost = (
     stones: number[],
     memo: number[][]
 ): number => {
-    if (start === end) return 0;
-    if (start > end) return Infinity;
-    if (memo[start][k] !== undefined) return memo[start][k];
+    // Base cases:
+    if (start === end) return 0; // We are already at the end.
+    if (start > end) return Infinity; // Cannot reach the end.
+    if (memo[start][k] !== undefined) return memo[start][k]; // Return memoized result if available.
 
     let result = Infinity;
+
+    // Try making 'k' jumps from the current position.
     for (let i = 1; i <= k; i++) {
         const nextStoneIndex = start + i;
+
+        // Ensure we don't go beyond the 'end' index.
         if (nextStoneIndex <= end) {
             const cost = calculateMinimumCost(
                 nextStoneIndex,
@@ -20,18 +37,21 @@ const calculateMinimumCost = (
                 stones,
                 memo
             ) + Math.abs(stones[start] - stones[nextStoneIndex]);
+
+            // Update the result with the minimum cost among all possible jumps.
             result = Math.min(result, cost);
         }
     }
 
+    // Memoize the result for this position and remaining jumps.
     memo[start][k] = result;
     return result;
 };
 
-const i = 0;
-const n = 5;
-const k = 3;
-const stones = [10, 30, 40, 50, 20];
-// This problem is a one-based index, so we need to start from index 1
+const i = 0; // Starting index
+const n = 5; // Ending index (adjusted to be zero-based)
+const k = 3; // Maximum number of jumps allowed
+const stones = [10, 30, 40, 50, 20]; // Array of stone positions
+// Initialize a memoization table with dimensions [n][k+1].
 const memo: number[][] = new Array(n).fill(null).map(() => new Array(k + 1));
-console.log(calculateMinimumCost(i, n - 1, k, stones, memo));
+console.log(calculateMinimumCost(i, n - 1, k, stones, memo)); // Calculate and log the minimum cost.
