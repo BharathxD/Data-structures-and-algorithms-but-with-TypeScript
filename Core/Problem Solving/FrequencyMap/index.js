83e438d682cd90d0fd26458a77d9730d60111ccd
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-09-12 21:37:26 +0530

    chore: improve frequency map problem

--- Core/Problem Solving/FrequencyMap/index.js
+++ Core/Problem Solving/FrequencyMap/index.js
@@ -1,17 +1,23 @@
 const frequency = (value) => {
   let obj = {};
   for (let val of value) {
-    if (!obj[val]) obj[val] = 1;
-    else obj[val]++;
+    if (!obj[val]) {
+      obj[val] = 1;
+    } else {
+      obj[val]++;
+    }
   }
   return obj;
 };
 
 const uniqueKeys = (arrayOfDuplicateValues) => {
   const nonDuplicateValues = {};
-  for (const property of arrayOfDuplicateValues)
-    if (!nonDuplicateValues[property]) nonDuplicateValues[property] = true;
-  return nonDuplicateValues;
+  for (const property of arrayOfDuplicateValues) {
+    if (!nonDuplicateValues[property]) {
+      nonDuplicateValues[property] = true;
+    }
+  }
+  return Object.keys(nonDuplicateValues);
 };
 
 console.log(uniqueKeys(["a", "b", "c", "d", "b", "c", "g", "e"]));
