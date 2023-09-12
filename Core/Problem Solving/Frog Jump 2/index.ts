b0d4f04286ffa0afd8a6407f4500085199dc548f
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-09-12 21:36:28 +0530

    add: refactor the frog jump 2 problem

--- Core/Problem Solving/Frog Jump 2/index.ts
+++ Core/Problem Solving/Frog Jump 2/index.ts
@@ -1,19 +1,37 @@
-const calculateMinimumCost2 = (start: number, end: number, k: number, stones: number[]): number => {
+const calculateMinimumCost = (
+    start: number,
+    end: number,
+    k: number,
+    stones: number[],
+    memo: number[][]
+): number => {
     if (start === end) return 0;
     if (start > end) return Infinity;
+    if (memo[start][k] !== undefined) return memo[start][k];
+
     let result = Infinity;
     for (let i = 1; i <= k; i++) {
-        result = Math.min(
-            result,
-            calculateMinimumCost2(start + i, end, k, stones) + Math.abs(stones[start] - stones[start + i]),
-        );
+        const nextStoneIndex = start + i;
+        if (nextStoneIndex <= end) {
+            const cost = calculateMinimumCost(
+                nextStoneIndex,
+                end,
+                k,
+                stones,
+                memo
+            ) + Math.abs(stones[start] - stones[nextStoneIndex]);
+            result = Math.min(result, cost);
+        }
     }
+
+    memo[start][k] = result;
     return result;
-}
+};
 
 const i = 0;
 const n = 5;
 const k = 3;
-const stones = [10, 30, 40, 50, 20]
-// This problem is a one based index, so we need to start from the index 1
-console.log(calculateMinimumCost2(i, n, k, stones));
+const stones = [10, 30, 40, 50, 20];
+// This problem is a one-based index, so we need to start from index 1
+const memo: number[][] = new Array(n).fill(null).map(() => new Array(k + 1));
+console.log(calculateMinimumCost(i, n - 1, k, stones, memo));
